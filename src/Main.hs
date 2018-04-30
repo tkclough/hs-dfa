b7d4393fe6806f456e753b881ba51fd104b5f57f
Tim Clough <tmclough98@gmail.com>
2018-04-29 22:33:12 -0400

    whoops add main for real, cabal

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,14 @@ import DFA.DFA
 import DFA.Parser 
 import DFA.QuickCheck
 
+import Test.QuickCheck 
+import System.Environment
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  [re, fd] <- getArgs
+  eitherDfa <- defaultParseDFAFile fd 
+
+  case eitherDfa of 
+    Left e -> print e
+    Right dfa -> verboseCheck (propRegex re dfa)
